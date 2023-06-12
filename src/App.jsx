1fa8a0433cd23f8cebfae6209e4658c267022bd6
1amSumit <sj779619@gmail.com>
2023-06-12 19:49:04 +0530

    Router implemted

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./UI/Home";
+import Menu from "./features/menu/Menu";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+]);
+
 const App = () => {
-  return <div>Hello from Vite!</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
